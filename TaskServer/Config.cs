7f4ac3ce727448ba5de117ed9f8a81ebee040510
BellCrow <peremptor1@gmx.de>
2016-11-18 18:53:12 +0100

    added error handler and expanded config and dbconnector

---
+++ TaskServer/Config.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskServer
+{
+    class Config
+    {
+        //private fields
+        string _dbDatabaseName;
+        string _dbAddress;
+        string _dbUserName;
+        string _commandsPath;
+        string _workingDirectory;
+        char _seperator = ';';
+
+        
+  
+        //end private fields
+
+        //getter setter for private fields
+        public string DbAddress
+        {
+            get { return _dbAddress; }
+            set { _dbAddress = value; }
+        }
+        public string DbUserName
+        {
+            get { return _dbUserName; }
+            set { _dbUserName = value; }
+        }
+        public string DbDatabaseName
+        {
+            get { return _dbDatabaseName; }
+            set { _dbDatabaseName = value; }
+        }
+        public string CommandsPath
+        {
+            get { return _workingDirectory+@"\"+_commandsPath; }
+            set { _commandsPath = value; }
+        }
+        public string WorkingDirectory
+        {
+            get { return _workingDirectory; }
+            set { _workingDirectory = value; }
+        }
+        public char Seperator
+        {
+            get { return _seperator; }
+            set { _seperator = value; }
+        }
+        //end getter setter
+        public Config()
+        {
+            _dbAddress = "127.0.0.1";
+            _dbDatabaseName = "taskUser";
+            _dbUserName = "taskUser";
+            _commandsPath = "commands.txt";
+            _workingDirectory = Directory.GetCurrentDirectory();
+        }
+        
+    }
+}
---
+++ TaskServer/ErrorHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskServer
+{
+    public class ErrorHandler
+    {
+        Config _conf;
+        public ErrorHandler(Config conf)
+        {
+            _conf = conf;
+        }
+    }
+    //place for custom exceptions
+    public class DBConnectorException : Exception
+    {
+        string _exceptionString;
+        public DBConnectorException(string exceptionString)
+        {
+            _exceptionString = exceptionString;
+        }
+    }
+    public class ConfigException : Exception
+    {
+        string _exceptionString;
+
+        public ConfigException(string exceptionstring)
+        {
+            _exceptionString = exceptionstring;
+        }
+    }
+}
