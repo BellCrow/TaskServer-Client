1f68b34d9c6da4f45b02e7d3fafefc518ae130db
BellCrow <peremptor1@gmx.de>
2017-02-04 23:51:25 +0100

    reowrkedMessagelibChanges into test main

--- TaskServer/Program.cs
+++ TaskServer/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Threading;
 using Messages;
+using System.Net;
 
 
 namespace TaskServer
@@ -19,21 +20,36 @@ namespace TaskServer
             conf.DbAddress = "127.0.0.1";
             conf.DbDatabaseName = "taskdatabase";
             conf.DbUserName = "taskuser";
-            string pw = ;
-            MessageCommunicator mec = new MessageCommunicator(50000);
+            string pw = "notarealpassword";
+            Connector serv = new Connector(IPAddress.Parse("127.0.0.1"),50000);
+            
 
             DBConnector db = new DBConnector(conf);
 
-            db.connectToDataBase(ref pw);
-            mec.listenForClient();
+            if(!db.connectToDataBase(ref pw))
+            {
+                Console.WriteLine("Could not connect to database:\nPress any button to exit Program:");
+                Console.ReadKey();
+                return;
+            }
+            MessageCommunicator mec = new MessageCommunicator(serv.listenForClient());
+            MessageCommunicator mec2 = new MessageCommunicator(serv.listenForClient());
+
             Console.WriteLine("MAIN:Got first Client\nStarting Echo Worker");
             Worker w = new Worker(mec, new EchoTask(),db);
             w.startWork();
+
+            Worker w2 = new Worker(mec2, new PrintContentTask(), db);
+            w2.startWork();
+
             while(mec.isWorking())
             {
                 Thread.Sleep(5000);
                 Console.WriteLine("MAIN:Worker still working");
             }
+
+
+            
         }
     }
 }
